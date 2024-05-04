e11deeaa5ca024b6c037258cdf85c27f56adf107
Jonda-HR <jonathan@contadito.com>
2024-05-03 22:11:24 -0600

    First commit

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { envs } from './config/envs';
+import { Logger } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const logger = new Logger('Main-Orders');
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      },
+    },
+  );
+
+  // app.setGlobalPrefix('api');
+
+  await app.listen();
+
+  logger.log(`Orders running on port ${envs.port}`);
+}
+bootstrap();
